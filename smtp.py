3ce30086ea2a61bf6aa7e240459956a52515520a
Ethem Turgut <48857416+ethmtrgt@users.noreply.github.com>
2020-10-30 22:14:08 +0300

    Update smtp.py
    I changed the sender credentials list to dictionary. And I made minor changes

--- smtp.py
+++ smtp.py
@@ -8,29 +8,23 @@ class mail(object):
         self.receiver = receiver
         
     def send(self):
-        mail = smtplib.SMTP('smtp.gmail.com',587)
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
         mail.starttls()
         mail.ehlo()
-        mail.login(self.sender[0],self.sender[1])
+        mail.login(self.sender['address'], self.sender['password'])
+        
         for receiver in self.receiver:
-            mail.sendmail(self.sender[0],receiver,self.content)
+            mail.sendmail(self.sender[0], receiver, self.content)
             
 
-sender = ['mail_address', 'address_pass']
-content = 'content'
-receiver = ['1@example.com', '2@example.com']
+sender = {
+    'address': 'ethmtrgt@gmail.com',
+    'password': 'thisIsMyPassw0rd'
+}
+
+# Example
+
+content = 'This mail sent from a python script.'
+receiver = ['test@gmail.com']
 sendMail = mail(sender, content, receiver)
 sendMail.send()
-
-    
-
-#content = 'this mail sended with python'#mail.starttls()
-#mail.ehlo()
-
-#fromAddr = 'mail_Address'
-#fromAddrPass = 'mail_Pass'
-#toAddrs = ['mail1@blabla.com', 'mail2@blabla.com', 'mail3@blabla.com']
-
-#mail.login(fromAddr, fromAddrPass)
-#for toAddr in toAddrs:
-#    mail.sendmail(fromAddr, toAddr, content)
